d35bccc27c42f5dcebed26cf65a01fa6cdab3da4
nurhadiansyah121 <118668386+nurhadiansyah121@users.noreply.github.com>
2022-11-27 21:37:58 +0700

    Update latihan7_biodata.php

--- NURHADIANSYAH - 2170231016 - Praktikum Pemrograman Web/Pertemuan 4/Latihan/latihan7_biodata.php
+++ NURHADIANSYAH - 2170231016 - Praktikum Pemrograman Web/Pertemuan 4/Latihan/latihan7_biodata.php
@@ -39,8 +39,8 @@
     echo "<center><form method='POST'>";
     echo "<h1>BIO DATA</h1>";
     echo "<table width='60%'>";
-    echo "<tr><td class='atas' width='30%'>Nama :</td><td width='30%'><input type='text' name='name' value='Sumardi'></td></tr>";
-    echo "<tr><td class='atas'>NIM :</td><td><input type='text' name='name' value='2170236003'></td></tr>";
+    echo "<tr><td class='atas' width='30%'>Nama :</td><td width='30%'><input type='text' name='name' value='Nurhadiansyah'></td></tr>";
+    echo "<tr><td class='atas'>NIM :</td><td><input type='text' name='name' value='2170231016'></td></tr>";
     echo "<tr><td class='atas'>Kelas :</td><td><input type='text' name='name' value='P2K'></td></tr>";
     echo "<tr><td class='atas'>Program Study :</td><td><input type='text' name='name' value='Teknik Informatika'></td></tr>";
     echo "<tr><td class='atas'>Agama :</td><td><select name='agama'>
@@ -53,7 +53,7 @@
     echo "<tr><td class='atas'>Jenis Kelamin :</td><td><input type='radio' name='jenis_kelamin' value='l'>Laki-laki
     <input type='radio' name='jenis_kelamin' value='p'>Perempuan</td></tr>";
     echo "<tr><td class='atas'>E-Mail :</td><td><input type='email' name='email' value='biodata@gmail.com'></td></tr>";
-    echo "<tr><td class='atas'>Foto :</td><td><img src='sumardi.png' width='200px'></img></td></tr>";
+    echo "<tr><td class='atas'>Foto :</td><td><img src='iyan.jpg' width='200px'></img></td></tr>";
     echo "<tr><td class='atas'>&nbsp;</td><td class='atas'><center><input type='submit' value=' Simpan '> <input type='reset' value=' Cancel '></center></td></tr>";
     echo "</table>";
     echo "</form></center>";
